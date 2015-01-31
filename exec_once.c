90c902f2f28b916c2b3a52f5a256d2ae41bf9823
wcy123 <wcy123@gmail.com>
2015-01-31 20:54:39 +0800

    rewrite execute transform unit.

--- exec_once.c
+++ exec_once.c
@@ -5,120 +5,34 @@
 static int exec_once_debug = 0;
 static void print_tu(FILE * fp, exec_once_tu_t * tu);
 exec_once_tu_t * g_exec_once = (void*)0;
-void exec_once_register(exec_once_block_t * x, exec_once_block_t** glist)
-{
-    if(exec_once_debug >=9){
-        fprintf(stderr,"%s:%d: register exec once block.\n",
-                x->file,x->line);
-    }
-    if(*glist == 0){
-        *glist = x;
-    }else{
-        exec_once_block_t * p = *glist;
-        while(p->next != 0) p = p->next;
-        p->next = x;
-    }
-}
-static inline void exec_once_run_tu(exec_once_tu_t* current,exec_once_tu_t * head);
-static inline void exec_once_run(exec_once_block_t * p)
-{
-    while(p){
-        if(exec_once_debug >8){
-            fprintf(stderr, "%s:%d:[%s] start running:\n",
-                    p->file,
-                    p->line,
-                    p->name);
-
-        }
-        p->f();
-        if(exec_once_debug>8){
-            fprintf(stderr, "%s:%d:[%s] end:\n",
-                    p->file,
-                    p->line,
-                    p->name);
-
-        }
-        p = p->next;
-    }
-}
-static inline int exec_once_depend_on(exec_once_tu_t* current, exec_once_tu_t* p)
-{
-    int ret = 0;
-    if(!p->done){
-        for(int i = 0; current->depend[i] != 0; ++i){
-            if(strcmp(current->depend[i],p->name)  == 0){
-                ret = 1; break;
-            }
-        }
-    }
-    return ret;
-}
-static inline void exec_once_run_dependency(exec_once_tu_t* current, exec_once_tu_t * head)
-{
-    exec_once_tu_t* p = head;
-    while(p!=0){
-        if(exec_once_depend_on(current,p)){
-            if(exec_once_debug){
-                fprintf(stderr,__FILE__ ":%d:[%s] %s depends on %s, running %s\n", __LINE__, __FUNCTION__,
-                        current->name,p->name,p->name);
-            }
-            exec_once_run_tu(p,head);
-        }
-        p = p->next;
-    }
-    return;
-}
-static inline void exec_once_run_tu(exec_once_tu_t* current,exec_once_tu_t * head)
-{
-    if(current->done){
-        if(0 && exec_once_debug){
-            fprintf(stderr,__FILE__ ":%d:[%s] `%s' was already done, cancelled\n", __LINE__, __FUNCTION__,
-                    current->name);
-        }
-        return;
-    }
-    current->done = 1;
-    if(exec_once_debug){
-        fprintf(stderr,__FILE__ ":%d:[%s] checking dependency for `%s'\n", __LINE__, __FUNCTION__,
-                current->name);
-    }
-    exec_once_run_dependency(current,head);
-    if(exec_once_debug){
-        fprintf(stderr,__FILE__ ":%d:[%s] `%s' start running\n", __LINE__, __FUNCTION__,
-                current->name);
-    }
-    exec_once_run(*current->head);
-    return;
-}
 static void check_name_confict(exec_once_tu_t * head);
 static void check_exec_block(exec_once_tu_t * head);
 static void resolve_dependency(exec_once_tu_t * head);
 static void check_dependency_exists(exec_once_tu_t * head);
 static void check_cycly_dependency(exec_once_tu_t * head);
+static void execute_tu(exec_once_tu_t* current);
+static int is_over(exec_once_tu_t * head);
 void exec_once_init()
 {
     exec_once_tu_t * head = g_exec_once;
-    g_exec_once = NULL;
     exec_once_tu_t* p = head;
     check_name_confict(head);
     check_exec_block(head);
     resolve_dependency(head);
     check_dependency_exists(head);
     check_cycly_dependency(head);
-    p = head;
     if(exec_once_debug){
         fprintf(stderr,__FILE__ ":%d:[%s] the scheduled list for exec_once:\n", __LINE__, __FUNCTION__);
-        while(p!=0){
+        for(p=head;p;p=p->next){
             if(!p->done){
-                fprintf(stderr,__FILE__ ":%d:[%s]      %s\n", __LINE__, __FUNCTION__,p->name);
+                print_tu(stderr,p);
             }
-            p = p->next;
         }
     }
-    p = head;
-    while(p!=0){
-        exec_once_run_tu(p,head);
-        p = p->next;
+    while(!is_over(head)){
+        for(p=head;p;p=p->next){
+            execute_tu(p);
+        }
     }
 }
 static void resolve_dependency(exec_once_tu_t * head)
@@ -242,6 +156,7 @@ static void check_cycly_dependency(exec_once_tu_t * head)
             strongconnect(&index,&S,p,&ok);
         }
     }
+    if(!ok) abort();
 }
 static void strongconnect(int * index, exec_once_tu_t **S, exec_once_tu_t * v, int * ok)
 {
@@ -289,7 +204,7 @@ static void strongconnect(int * index, exec_once_tu_t **S, exec_once_tu_t * v, i
         if(*S == v){
             // OK, single element CSS
         }else{
-            ok = 0;
+            *ok = 0;
             fprintf(stderr,"EXEC_ONCE: cycle dependency detected for the following TU.\n");
             exec_once_tu_t * q = *S;
             do{
@@ -307,6 +222,98 @@ static void strongconnect(int * index, exec_once_tu_t **S, exec_once_tu_t * v, i
         }while(w!=v);
     }
     return;
+}
+static void execute_blocks(exec_once_block_t * p);
+static int execute_dependency(exec_once_tu_t* current);
+static void execute_tu(exec_once_tu_t* current)
+{
+    if(current->done){
+        if(0 && exec_once_debug){
+            fprintf(stderr,__FILE__ ":%d:[%s] `%s' was already done, cancelled\n", __LINE__, __FUNCTION__,
+                    current->name);
+        }
+        return;
+    }
+    current->done = 1;
+    int ok = execute_dependency(current);
+    if(!ok){
+        // depenency is running, cancelled.
+        current->done = 0;
+    }else{
+        if(exec_once_debug){
+            fprintf(stderr,__FILE__ ":%d:[%s] `%s' start running\n", __LINE__, __FUNCTION__,
+                    current->name);
+        }
+    }
+    execute_blocks(*current->head);
+    current->done = 2;
+    return;
+}
+static int execute_dependency(exec_once_tu_t* p)
+{
+    int ret = 1;
+    for(int i = 0; p->depend[i] != 0; ++i){
+        if(p->depend[i][0] != '\0'){
+            fprintf(stderr, "logcial error.\n"); abort();
+        }
+        exec_once_tu_t * q = (exec_once_tu_t*)p->depend[i];
+        switch(q->done){
+        case 0:
+            execute_tu(q); break;
+        case 1: // if dependency is running; cancelled
+            ret = 0; break;
+        case 2: // if dependency is already done. do nothing
+            break;
+        default: abort();
+        }
+    }
+    return ret;
+}
+static void execute_blocks(exec_once_block_t * p)
+{
+    while(p){
+        if(exec_once_debug >8){
+            fprintf(stderr, "%s:%d:[%s] start running:\n", p->file, p->line, p->name);}
+        p->f();
+        if(exec_once_debug>8){
+            fprintf(stderr, "%s:%d:[%s] end:\n", p->file, p->line, p->name);
+        }
+        p = p->next;
+    }
+}
+void exec_once_register(exec_once_block_t * x, exec_once_block_t** glist)
+{
+    if(exec_once_debug >=9){
+        fprintf(stderr,"%s:%d: register exec once block.\n",
+                x->file,x->line);
+    }
+    if(*glist == 0){
+        *glist = x;
+    }else{
+        exec_once_block_t * p = *glist;
+        while(p->next != 0) p = p->next;
+        p->next = x;
+    }
+}
+static int is_over(exec_once_tu_t * head)
+{
+    int ret = 1;
+    for(exec_once_tu_t * p  = head; p ; p = p->next){
+        switch(p->done){
+        case 0:
+            ret = 0; break; // not over
+        case 1:
+            // if it is running; reentrance, over.
+            // let previous exec_once_init to
+            break;
+        case 2:
+            // it is done. OK. continue to check others
+            break;
+        default: abort();
+        }
+        if(!ret) break;
+    }
+    return ret;
 }
 __attribute__((constructor))
 static void __exec_once_init_self()
